be475bf26745acb3ecbe34e5611d1df9ae6a003c
Matthew Hayes <mhayes@uship.com>
2015-10-25 12:18:41 -0500

    initial commit

---
+++ CarthageExampleFramework/CarthageExample.swift
@@ -0,0 +1,17 @@
+//
+//  CarthageExample.swift
+//  CarthageExampleFramework
+//
+//  Created by Matthew Hayes on 10/25/15.
+//  Copyright © 2015 cruinh.net. All rights reserved.
+//
+
+import Foundation
+
+public class CarthageExample
+{
+    public func foo()
+    {
+        print("it worked")
+    }
+}
---
+++ CarthageExampleFramework/CarthageExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  CarthageExampleFramework.h
+//  CarthageExampleFramework
+//
+//  Created by Matthew Hayes on 10/25/15.
+//  Copyright © 2015 cruinh.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageExampleFramework.
+FOUNDATION_EXPORT double CarthageExampleFrameworkVersionNumber;
+
+//! Project version string for CarthageExampleFramework.
+FOUNDATION_EXPORT const unsigned char CarthageExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageExampleFramework/PublicHeader.h>
+
+
